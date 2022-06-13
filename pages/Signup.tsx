fb5999cebefb2d316e8413adfb69e745876ab1b5
Simo Echaarani <echaaranimohamed@gmail.com>
2022-06-13 13:58:39 +0100

    adding some text to signup form

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -13,6 +13,40 @@ const Signup = () => {
         <Input name="fullName" placeholder="Full Name" />
         <Input name="username" placeholder="Username" />
         <Input name="Password" placeholder="Passwonrd" />
+        <p
+          style={{
+            color: "var(--txt-c-2)",
+            fontSize: "1.2rem",
+            textAlign: "center",
+            margin: "2rem 0",
+          }}
+        >
+          People who use our service may have uploaded your contact information
+          to Instagram.
+          <strong>
+            <a href="/" target="_blank">
+              {" "}
+              Learn More
+            </a>
+          </strong>
+          <br />
+          <br />
+          By signing up, you agree to our
+          <strong>
+            <a href="/" target="_blank">
+              Terms
+            </a>
+            ,
+            <a href="/" target="_blank">
+              Data Policy
+            </a>{" "}
+            and
+            <a href="/" target="_blank">
+              Cookies Policy
+            </a>{" "}
+            .
+          </strong>
+        </p>
         <WideButton hasIcon={false}>Sign up</WideButton>
       </form>
     </SignInUpContainer>

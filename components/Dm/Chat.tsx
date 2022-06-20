8de42e6d4255d6d2f21da34cfa858c9ccc8fa630
Simo Echaarani <echaaranimohamed@gmail.com>
2022-06-20 16:21:45 +0100

    Chat messages component added .

--- components/Dm/Chat.tsx
+++ components/Dm/Chat.tsx
@@ -3,6 +3,7 @@ import TextArea from "../Textarea";
 import styles from "./Dm.module.scss";
 import ChatDetailsIcon from "../../public/ChatDetails.svg";
 import ArrowLeft from "../../public/leftArrow.svg";
+import ProfilePic from "../ProfilePic";
 
 const Chat = () => {
   return (
@@ -18,7 +19,21 @@ const Chat = () => {
           <ChatDetailsIcon />
         </div>
       </div>
-      <div className={styles.chat}></div>
+      <div className={styles.chat}>
+        <MessageChat messageTxt="aji 3endi" IsMyMessage={true} />
+        <MessageChat
+          contactImgSrc="./baif.jpg"
+          messageTxt="okay safi 7ta nchof"
+          IsMyMessage={false}
+        />
+        <MessageChat messageTxt="la bla matchof" IsMyMessage={true} />
+        <MessageChat
+          contactImgSrc="./baif.jpg"
+          messageTxt="shaloom"
+          IsMyMessage={false}
+        />
+        <MessageChat messageTxt="wassaup" IsMyMessage={true} />
+      </div>
       <div className={styles.textAreaContainer}>
         <TextArea isCommentInput={false} />
       </div>
@@ -27,3 +42,26 @@ const Chat = () => {
 };
 
 export default Chat;
+
+type MessageChatProps = {
+  IsMyMessage: boolean;
+  messageTxt: string;
+  contactImgSrc?: string;
+};
+
+const MessageChat = ({
+  IsMyMessage,
+  messageTxt,
+  contactImgSrc = "",
+}: MessageChatProps) => {
+  return (
+    <div className={`${styles.messageChat} ${IsMyMessage && styles.myMessage}`}>
+      {!IsMyMessage && (
+        <div style={{ alignSelf: "end" }}>
+          <ProfilePic src={contactImgSrc} size="size-5" />
+        </div>
+      )}
+      <div className={styles.messageTxt}>{messageTxt}</div>
+    </div>
+  );
+};

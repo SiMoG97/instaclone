8d0212e0eaaecbb072cf282ec0d4a7f159b2b038
Simo Echaarani <echaaranimohamed@gmail.com>
2022-07-01 13:27:33 +0100

    adding react-hook-form and json-server dependencies

--- components/Navbar/Dropdown.tsx
+++ components/Navbar/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext } from "react";
 import styles from "./Navbar.module.scss";
 import ProfileIcon from "../../public/profile.svg";
 import BookmarkIcon from "../../public/bookmark.svg";

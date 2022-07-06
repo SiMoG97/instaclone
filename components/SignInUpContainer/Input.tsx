918e5ab5075c1d05ac4d62543acb6277600a1700
Simo Echaarani <echaaranimohamed@gmail.com>
2022-07-06 17:08:40 +0100

    signup form error and type handling

--- components/SignInUpContainer/Input.tsx
+++ components/SignInUpContainer/Input.tsx
@@ -1,19 +1,51 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+import { FieldError } from "react-hook-form";
 
 import styles from "./form.module.scss";
 
 type InputProps = {
   text: string;
   name: string;
+  error: FieldError | undefined;
   type?: string;
 };
 
 const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { name, type, text } = props;
+  const { name, type, text, error } = props;
+  const [show, setShow] = useState<"show" | "hide">("show");
   return (
-    <div className={`${styles.inputContainer}`}>
-      <input type={type} id={name} placeholder=" " ref={ref} {...props} />
+    <div className={`${styles.inputContainer} ${error && styles.inputError}`}>
+      <input
+        className={` ${type === "password" && styles.passwordInput}`}
+        autoComplete="off"
+        type={type}
+        id={name}
+        placeholder=" "
+        ref={ref}
+        {...props}
+      />
       <label htmlFor={name}>{text}</label>
+      {type === "password" && (
+        <span
+          className={styles.togglePass}
+          onClick={(e) => {
+            const passInput = e.currentTarget.offsetParent
+              ?.childNodes[0] as HTMLInputElement;
+            if (passInput) {
+              if (passInput.type === "password") {
+                passInput.type = "text";
+                setShow("hide");
+              } else {
+                passInput.type = "password";
+                setShow("show");
+              }
+            }
+          }}
+        >
+          {show}
+        </span>
+      )}
+      {error && <p className={styles.errorMessage}>{error.message}</p>}
     </div>
   );
 });
--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -1,16 +1,68 @@
 import { useForm } from "react-hook-form";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
+// const Cjoi = Joi.extend(require("joi-phone-number"));
 
 import Input from "../components/SignInUpContainer/Input";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import SignInUpContainer, {
   OrLine,
   WideButton,
 } from "../components/SignInUpContainer";
 import { SignFormTypes } from "../utils/GlobalTypes";
 
-const Signup = () => {
-  const { register, handleSubmit } = useForm<SignFormTypes>();
+const phonePatter =
+  /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/;
+const formSchema = Joi.object<SignFormTypes>({
+  phoneEmail: Joi.alternatives()
+    .try(
+      Joi.string()
+        .email({ tlds: { allow: false } })
+        .required(),
+      Joi.string().regex(RegExp(phonePatter)).required()
+    )
+    .required()
+    .messages({
+      "alternatives.match":
+        "This field should be either a phone number or email",
+    }),
 
+  fullName: Joi.string()
+
+    .min(6)
+    .max(30)
+    .required()
+    .messages({
+      "string.min": "'Full name' should be more than 6 letters",
+      "string.max": "'Full name' should be less than 30 letters",
+      "string.empty": "'Full name' should not be empty",
+    }),
+  userName: Joi.string()
+    .regex(RegExp(/^(?=[a-zA-Z0-9._]{0,100}$)(?!.*[_.]{2})[^_.].*[^_.]$/))
+    .min(6)
+    .max(20)
+    .required()
+    .messages({
+      "string.min": "'Username' should be more than 6 characters",
+      "string.max": "'Username' should be less than 30 characters",
+      "string.empty": "'Username' should not be empty",
+      "string.pattern.base": "this is not a valid username",
+    }),
+  password: Joi.string().min(8).max(64).required().messages({
+    "string.min": "'Password' should be more than 8 characters",
+    "string.max": "'Password' should be less than 64 characters ",
+    "string.empty": "'Password' should not be empty",
+  }),
+});
+
+const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignFormTypes>({
+    resolver: joiResolver(formSchema),
+  });
   return (
     <SignInUpContainer>
       <WideButton hasIcon={true}>Log in With Google</WideButton>
@@ -24,14 +76,27 @@ const Signup = () => {
           {...register("phoneEmail")}
           text="Phone number or email"
           name="phoneEmail"
+          error={errors.phoneEmail}
         />
-        <Input {...register("fullName")} text="Full Name" name="fullName" />
-        <Input {...register("userName")} text="Username" name="userName" />
+
+        <Input
+          {...register("fullName")}
+          text="Full Name"
+          name="fullName"
+          error={errors.fullName}
+        />
+        <Input
+          {...register("userName")}
+          text="Username"
+          name="userName"
+          error={errors.userName}
+        />
         <Input
           {...register("password")}
           type="password"
           text="Passwonrd"
           name="password"
+          error={errors.password}
         />
 
         <p

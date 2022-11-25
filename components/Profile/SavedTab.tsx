720d6d1246caf3af3e5aaefb8af340cdfc2194a7
Simo Echaarani <echaaranimohamed@gmail.com>
2022-11-25 16:34:20 +0100

    Saved and Tagged posts placeholder done

--- components/Profile/SavedTab.tsx
+++ components/Profile/SavedTab.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import styles from "./profile.module.scss";
+import SavedIcon from "../../public/profileIcons/savedIcon.svg";
 
 const SavedTab = () => {
-  return <div>SavedTab</div>;
+  return <>{false ? <h1>saved posts</h1> : <SavedTabPlaceholder />}</>;
+};
+const SavedTabPlaceholder = () => {
+  return (
+    <div className={styles.tabsContentContainer}>
+      <div
+        style={{
+          textAlign: "left",
+          fontSize: "1.3rem",
+          color: "var(--txt-c-2)",
+          marginBottom: "2rem",
+        }}
+      >
+        Only you can see what you've saved
+      </div>
+      <div className={styles.circle}>
+        <SavedIcon width="38" height="38" viewBox="0 0 24 24" />
+      </div>
+      <div className={styles.bigText}>Save</div>
+      <p>
+        Save photos and videos that you want to see again. No one is notified,
+        and only you can see what you've saved.
+      </p>
+    </div>
+  );
 };
 
 export default SavedTab;
--- components/Profile/TaggedTab.tsx
+++ components/Profile/TaggedTab.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import styles from "./profile.module.scss";
+import TaggedIcon from "../../public/profileIcons/taggedIcon.svg";
 
 const TaggedTab = () => {
-  return <div>TaggedTab</div>;
+  return (
+    <>
+      {false ? <h1>here is your taggged posts</h1> : <TaggedTabPlaceholder />}
+    </>
+  );
+};
+
+const TaggedTabPlaceholder = () => {
+  return (
+    <div className={styles.tabsContentContainer}>
+      <div className={styles.circle}>
+        <TaggedIcon width="38" height="38" viewBox="0 0 24 24" />
+      </div>
+      <div className={styles.bigText}>Photos of you</div>
+      <p>When people tag you in photos, they'll appear here.</p>
+    </div>
+  );
 };
 
 export default TaggedTab;

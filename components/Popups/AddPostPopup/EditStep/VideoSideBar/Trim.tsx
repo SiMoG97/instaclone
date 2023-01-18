1ab91f8cbff4dc04bf28595b4b4ef163233d0786
Simo Echaarani <echaaranimohamed@gmail.com>
2023-01-18 21:18:09 +0100

    video timeline thumbs added

--- components/Popups/AddPostPopup/EditStep/VideoSideBar/Trim.tsx
+++ components/Popups/AddPostPopup/EditStep/VideoSideBar/Trim.tsx
@@ -12,18 +12,33 @@ export default function Trim({ file, Vidframes }: TrimType) {
   return (
     <div className={styles.Trim}>
       <h3 className={styles.editSectionTitle}>Trim</h3>
-      <div>
+      <div className={styles.trimTimeline}>
         <ImagesPreview Vidframes={Vidframes} />
-        <div className={styles.trimTime}>
-          {Array.from({ length: 9 }, (_, i) => i).map((_, i) => (
-            <div
-              className={styles.dots}
-              key={i}
-              data-time={`${((file.duration / 9) * i) | 0}s`}
-            ></div>
-          ))}
+        <div className={styles.videoRangeContainer}>
+          <div className={`${styles.startsFrom} ${styles.darkArea}`}>
+            <div className={`${styles.thumb} ${styles.leftThumb}`}></div>
+          </div>
+          <div className={styles.selectedRange}></div>
+          <div className={`${styles.endsAt} ${styles.darkArea}`}>
+            <div className={`${styles.thumb} ${styles.rightThumb}`}></div>
+          </div>
         </div>
       </div>
+      <BottomTimeTrim duration={file.duration} />
+    </div>
+  );
+}
+
+function BottomTimeTrim({ duration }: { duration: number }) {
+  return (
+    <div className={styles.trimTime}>
+      {Array.from({ length: 9 }, (_, i) => i).map((_, i) => (
+        <div
+          className={styles.dots}
+          key={i}
+          data-time={`${((duration / 9) * i) | 0}s`}
+        ></div>
+      ))}
     </div>
   );
 }
